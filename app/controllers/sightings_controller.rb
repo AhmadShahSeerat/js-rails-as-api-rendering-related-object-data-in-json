b41eaafe08ee230efe0d867a968911916b7e26ab
Ahmad Shah <ah.seerat@gmail.com>
2022-11-14 19:29:51 -0500

    Done.

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,4 +1,19 @@
 def show
     sighting = Sighting.find_by(id: params[:id])
-    render json: { id: sighting.id, bird: sighting.bird, location: sighting.location }
+    render json: sighting.to_json(:include =>
+    {
+      :bird => 
+      {
+         :only => 
+         [:name, :species]
+      },
+      :location =>
+      {
+        :only =>
+        [:latitude, :longtitude]
+      }
+    }, :except =>
+                [:updated_at])
   end
+
+  
--- db/schema.rb
+++ db/schema.rb
@@ -10,29 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_14_220731) do
-
-  create_table "birds", force: :cascade do |t|
-    t.string "name"
-    t.string "species"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "locations", force: :cascade do |t|
-    t.float "latitude"
-    t.float "longitude"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "sightings", force: :cascade do |t|
-    t.integer "bird_id"
-    t.integer "location_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["bird_id"], name: "index_sightings_on_bird_id"
-    t.index ["location_id"], name: "index_sightings_on_location_id"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
